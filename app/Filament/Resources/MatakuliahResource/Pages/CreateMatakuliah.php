46f9b4a0e6e594920b9f5015b42d3b34d1cc0130
Raihan Presiden Washil <108395093+presidenwashil@users.noreply.github.com>
2024-03-01 05:40:40 +0000

    Add MatakuliahResource and its related pages

---
+++ app/Filament/Resources/MatakuliahResource/Pages/CreateMatakuliah.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Resources\MatakuliahResource\Pages;
+
+use App\Filament\Resources\MatakuliahResource;
+use Filament\Actions;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateMatakuliah extends CreateRecord
+{
+    protected static string $resource = MatakuliahResource::class;
+}
