bb3c3eb0075dae13be782bf63521b0603577014b
Raihan Presiden Washil <108395093+presidenwashil@users.noreply.github.com>
2024-03-27 02:43:40 +0000

    Add users route and navigation link

--- resources/views/livewire/layout/navigation.blade.php
+++ resources/views/livewire/layout/navigation.blade.php
@@ -36,6 +36,12 @@ new class extends Component
                 </div>
 
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                    <x-nav-link :href="route('users')" :active="request()->routeIs('users')" wire:navigate>
+                        {{ __('Users') }}
+                    </x-nav-link>
+                </div>
+
+                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
                     <x-nav-link :href="route('jadwal')" :active="request()->routeIs('jadwal')" wire:navigate>
                         {{ __('Jadwal') }}
                     </x-nav-link>
@@ -89,6 +95,10 @@ new class extends Component
         <div class="pt-2 pb-3 space-y-1">
             <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')" wire:navigate>
                 {{ __('Dashboard') }}
+            </x-responsive-nav-link>
+
+            <x-responsive-nav-link :href="route('users')" :active="request()->routeIs('users')" wire:navigate>
+                {{ __('Users') }}
             </x-responsive-nav-link>
 
             <x-responsive-nav-link :href="route('jadwal')" :active="request()->routeIs('jadwal')" wire:navigate>
